ebb6099b2a709310332bff50447b74d6dc2b12b6
yoav_zibin <yoav.zibin@gmail.com>
2018-02-10 06:56:52 -0500

    x

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
