00f262c4c6ca6bf80d4fe0adb8d11f905befe6fc
yoav_zibin <yoav.zibin@gmail.com>
2018-02-23 17:28:32 -0500

    x

--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -1,11 +1,13 @@
 import { Reducer } from 'redux';
-import { StoreState } from '../types';
+import { StoreState, GameInfo } from '../types';
 import { storeStateDefault } from '../stores/defaults';
 
 export const reducer: Reducer<StoreState> = 
   (state: StoreState = storeStateDefault, action: any) => {
-  if (action.type === 'SOME_PARTICULAR_TYPE_HERE') {
-    return state;
+  if (action.type === 'SET_GAMES_LIST') {
+    let newGameList: GameInfo[] = action.gamesList;
+    let {gamesList, ...rest} = state;
+    return {gamesList: newGameList, ...rest};
   } else {
     return state;
   }
