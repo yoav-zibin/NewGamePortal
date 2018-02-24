be811c431f98d8ea096f0afd33499d4bdf3d09a2
yoav_zibin <yoav.zibin@gmail.com>
2018-02-24 18:13:56 -0500

    x

---
+++ src/reducers/index.test.ts
@@ -0,0 +1,38 @@
+/**
+ * @jest-environment node
+ */
+import { reducer, Action } from './index';
+import { storeStateDefault } from '../stores/defaults';
+import { GameInfo, Image, StoreState } from '../types';
+
+const image: Image = {
+  imageId: 'someImageId',
+  downloadURL: 'https://someurl.com/foo.png',
+  height: 1024,
+  width: 700,
+  isBoardImage: true,
+};
+
+const gameInfo: GameInfo = {
+  gameSpecId: 'someId',
+  gameName: 'Some game name',
+  screenShoot: image,
+};
+
+function reduce(state: StoreState, action: Action): StoreState {
+  return reducer(state, <any> action);
+}
+
+it('get initial state', () => {
+  expect(reduce(<any> undefined, {})).toEqual(storeStateDefault);
+});
+
+it('setGamesList', () => {
+  let gamesList = [gameInfo];
+  let action: Action = {
+    setGamesList: gamesList
+  };
+  let initialState = storeStateDefault;
+  let expectedState = Object.assign(storeStateDefault, {gamesList: gamesList});
+  expect(reduce(initialState, action)).toEqual(expectedState);
+});
--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -6,7 +6,7 @@ import {
 } from '../types';
 import { storeStateDefault } from '../stores/defaults';
 
-interface Action {
+export interface Action {
   setGamesList?: GameInfo[];
   updateGameSpecs?: GameSpecs;
   setMatchesList?: MatchInfo[];
@@ -24,21 +24,49 @@ function mergeMaps<T>(
   return Object.assign(original, updateWithEntries);
 }
 
+function checkCondition(desc: string, cond: boolean) {
+  if (!cond) {
+    throw new Error('Condition check failed for: ' + desc);
+  }
+}
+
+function checkStoreInvariants(state: StoreState) {
+  // TODO: check invariants, e.g., that every Image object in the store is also present in
+  // store.gameSpecs.imageIdToImage.
+  
+  checkCondition(
+    'currentMatchIndex is in range',
+    state.currentMatchIndex >= -1 && state.currentMatchIndex <= state.matchesList.length);
+}
+
+function reduce(state: StoreState, action: Action) {
+  if (action.setGamesList) {
+    let { gamesList, ...rest } = state;
+    return { gamesList: action.setGamesList, ...rest };
+
+  } else if (action.setMatchesList) {
+    let { matchesList, ...rest } = state;
+    return { matchesList: action.setMatchesList, ...rest };
+
+    // TODO: support all other reducers.
+
+  } else if (action.updateGameSpecs) {
+    let {imageIdToImage, elementIdToElement, gameSpecIdToGameSpec} = action.updateGameSpecs;
+    let { gameSpecs, ...rest } = state;
+    return { gameSpecs: {
+        imageIdToImage: mergeMaps(gameSpecs.imageIdToImage, imageIdToImage), 
+        elementIdToElement: mergeMaps(gameSpecs.elementIdToElement, elementIdToElement), 
+        gameSpecIdToGameSpec: mergeMaps(gameSpecs.gameSpecIdToGameSpec, gameSpecIdToGameSpec), 
+      }, ...rest };
+  } else {
+    return state;
+  }
+}
+
 export const reducer: Reducer<StoreState> =
   (state: StoreState = storeStateDefault, actionWithAnyType: any) => {
-    const action: Action = actionWithAnyType;
-    if (action.setGamesList) {
-      let { gamesList, ...rest } = state;
-      return { gamesList: action.setGamesList, ...rest };
-    } else if (action.updateGameSpecs) {
-      let {imageIdToImage, elementIdToElement, gameSpecIdToGameSpec} = action.updateGameSpecs;
-      let { gameSpecs, ...rest } = state;
-      return { gameSpecs: {
-          imageIdToImage: mergeMaps(gameSpecs.imageIdToImage, imageIdToImage), 
-          elementIdToElement: mergeMaps(gameSpecs.elementIdToElement, elementIdToElement), 
-          gameSpecIdToGameSpec: mergeMaps(gameSpecs.gameSpecIdToGameSpec, gameSpecIdToGameSpec), 
-        }, ...rest };
-    } else {
-      return state;
-    }
+    checkStoreInvariants(state);
+    const newState = reduce(state, actionWithAnyType);
+    checkStoreInvariants(newState);
+    return newState;
   };
