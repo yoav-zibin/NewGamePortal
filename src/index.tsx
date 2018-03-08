e554cc4ac976cf583bd97ac5309cf90c58978104
Amanpreet Singh <apsdehal@gmail.com>
2018-03-08 15:06:56 -0500

    Update App and index for react-router

--- src/index.tsx
+++ src/index.tsx
@@ -2,17 +2,23 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import { Provider } from 'react-redux';
+import { Route, BrowserRouter } from 'react-router-dom';
 
 import { store } from './stores/index';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
-// TODO: Add support for react router. Follow GameBuilder for adding support for it.
+// TODO: In basename, add support for gh-pages baseURL
+// Either use config for that or use location href to decide
 ReactDOM.render(
   <MuiThemeProvider>
     <Provider store={store}>
-      <App />
+      <BrowserRouter basename="/">
+        <div>
+          <Route path="/" component={App} />
+        </div>
+      </BrowserRouter>
     </Provider>
   </MuiThemeProvider>,
   document.getElementById('root') as HTMLElement
