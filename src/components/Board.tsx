79734c45801b96b89d4b2f4bd319fefdd1dc8af5
Herbert Li <hl1785@nyu.edu>
2018-03-30 12:32:43 -0400

    Added tooltips

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,8 +1,15 @@
 import * as React from 'react';
 import { Layer, Stage } from 'react-konva';
-import { MatchInfo, GameSpec, Piece } from '../types';
+import { MatchInfo, GameSpec, Piece, CardVisibility } from '../types';
 import CanvasImage from './CanvasImage';
-import { AppBar, FlatButton } from 'material-ui';
+import {
+  AppBar,
+  FlatButton,
+  IconButton,
+  IconMenu,
+  MenuItem
+} from 'material-ui';
+import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
 import { connect } from 'react-redux';
 import { StoreState } from '../types/index';
 import { ourFirebase } from '../services/firebase';
@@ -15,14 +22,34 @@ interface BoardProps {
   myUserId: string;
 }
 
+interface BoardState {
+  showCardOptions: boolean;
+  showTooltip: boolean;
+}
+
 /**
  * A reusable board class, that given a board image and pieces in props
  * can draw the board and piece on top of it using konva.
  * Should also add drag and drop functionality later on.
  */
-class Board extends React.Component<BoardProps, {}> {
+class Board extends React.Component<BoardProps, BoardState> {
+  cardIndex: number;
+  selfParticipantIndex: number;
+  participantNames: string[];
+  deckIndex: number;
+  tooltipPosition: {
+    x: number;
+    y: number;
+  };
+  visibleTo: CardVisibility;
+  parentContainer: HTMLDivElement;
+
   constructor(props: BoardProps) {
     super(props);
+    this.state = {
+      showCardOptions: false,
+      showTooltip: false
+    };
   }
 
   // cycles through the images of each piece
@@ -42,6 +69,11 @@ class Board extends React.Component<BoardProps, {}> {
   rollDice(index: number) {
     // TODO:
     console.log('Roll Dice for index:', index);
+  }
+
+  shuffleDeck(index: number) {
+    // TODO:
+    console.log('Shufle Deck for index:', index);
   }
 
   handleCardClick(index: number) {
@@ -68,6 +100,56 @@ class Board extends React.Component<BoardProps, {}> {
     helper.dragTo(index, x, y);
     ourFirebase.updatePieceState(match, index);
   };
+
+  makeCardVisibleToSelf() {
+    // TODO:
+  }
+
+  makeCardVisibleToAll() {
+    // TODO:
+  }
+
+  makeCardHiddenToAll() {
+    // TODO:
+  }
+
+  toggleTooltip(refString: string, index: number) {
+    let parentContainerPosition = this.parentContainer.getBoundingClientRect() as DOMRect;
+    let canvasImage = this.refs[refString] as CanvasImage;
+    let position = canvasImage.imageNode.getAbsolutePosition();
+    // let width = canvasImage.imageNode.width();
+    let width = 0;
+    this.visibleTo = this.props.matchInfo.matchState[
+      index
+    ].cardVisibilityPerIndex;
+    this.tooltipPosition = {
+      x: parentContainerPosition.x + position.x + width,
+      y: parentContainerPosition.y + position.y
+    };
+    this.setState({ showTooltip: !this.state.showTooltip });
+  }
+
+  displayCardOptions(
+    cardIndex: number,
+    selfParticipantIndex: number,
+    participantNames: string[],
+    deckIndex: number
+  ) {
+    this.cardIndex = cardIndex;
+    this.selfParticipantIndex = selfParticipantIndex;
+    this.participantNames = participantNames;
+    this.deckIndex = deckIndex;
+    this.setState({
+      showTooltip: false,
+      showCardOptions: true
+    });
+  }
+
+  hideCardOptions() {
+    this.setState({
+      showCardOptions: false
+    });
+  }
 
   //   componentWillUpdate() {
   //     for (let i = 0; i < this.props.pieces.length; i++) {
@@ -111,6 +193,7 @@ class Board extends React.Component<BoardProps, {}> {
             } else if (kind === 'card') {
               this.handleCardClick(index);
             }
+            this.toggleTooltip('canvasImage' + index, index);
           }}
           onMouseOver={() => {
             // this.props.hideCardOptions();
@@ -155,6 +238,97 @@ class Board extends React.Component<BoardProps, {}> {
       );
     });
 
+    let toolTipLayer = (
+      <>
+        {this.state.showTooltip ? (
+          <IconMenu
+            iconButtonElement={
+              <IconButton>
+                <MoreVertIcon />
+              </IconButton>
+            }
+            anchorOrigin={{ horizontal: 'left', vertical: 'top' }}
+            targetOrigin={{ horizontal: 'left', vertical: 'top' }}
+            className="my-tooltip"
+            style={{
+              left: this.tooltipPosition.x,
+              top: this.tooltipPosition.y,
+              position: 'absolute',
+              zIndex: 100
+            }}
+          >
+            {Object.keys(this.visibleTo).length === 0 ? (
+              <MenuItem primaryText="Card is visible to no one." />
+            ) : null}
+            {Object.keys(this.visibleTo).map((name, index) => {
+              return (
+                <MenuItem
+                  key={'tooltip' + index}
+                  style={{ padding: '0', listStyle: 'none', margin: '0' }}
+                  primaryText={name}
+                />
+              );
+            })}
+          </IconMenu>
+        ) : null}
+        {this.state.showCardOptions ? (
+          <div
+            className="my-card-options"
+            style={{
+              left: this.tooltipPosition.x,
+              top: this.tooltipPosition.y
+            }}
+          >
+            <div
+              className="close-card-options"
+              onClick={() => {
+                this.hideCardOptions();
+              }}
+            >
+              x
+            </div>
+            <span style={{ textDecoration: 'underline', textAlign: 'center' }}>
+              OPTIONS:
+            </span>
+            <ul style={{ padding: '0', listStyle: 'none', margin: '0' }}>
+              <li
+                className="card-options-item"
+                onClick={() => {
+                  this.makeCardVisibleToSelf();
+                }}
+              >
+                Make Visible To me
+              </li>
+              <li
+                className="card-options-item"
+                onClick={() => {
+                  this.makeCardVisibleToAll();
+                }}
+              >
+                Make Visible To Everyone
+              </li>
+              <li
+                className="card-options-item"
+                onClick={() => {
+                  this.makeCardHiddenToAll();
+                }}
+              >
+                Hide From Everyone
+              </li>
+              <li
+                className="card-options-item"
+                onClick={() => {
+                  this.shuffleDeck(this.deckIndex);
+                }}
+              >
+                Shuffle Deck
+              </li>
+            </ul>
+          </div>
+        ) : null}
+      </>
+    );
+
     return (
       <>
         <AppBar
@@ -172,7 +346,8 @@ class Board extends React.Component<BoardProps, {}> {
           }
           title={<span>Match: {props.matchInfo.game.gameName}</span>}
         />
-        <div ref={() => 'parentContainer'}>
+        <div ref={(el: HTMLDivElement) => (this.parentContainer = el)}>
+          {toolTipLayer}
           <Stage width={width} height={height}>
             <Layer ref={() => 'boardLayer'}>{boardLayer}</Layer>
             <Layer ref={() => 'piecesLayer'}>{piecesLayer}</Layer>
