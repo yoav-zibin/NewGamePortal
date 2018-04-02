e1a3a3443a0455b23376480c4dc6e0eaf5b70465
sisi li <alicesqcg@gmail.com>
2018-04-03 03:36:09 +0800

    updated merge

--- src/components/ContactsList.tsx
+++ src/components/ContactsList.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react';
-// import {PhoneNumberToUserId, PhoneNumberToContact, Contact} from '../types';
 import { Contact } from '../types';
 import { List, ListItem } from 'material-ui/List';
 import Divider from 'material-ui/Divider';
@@ -13,50 +12,16 @@ const style = {
   marginRight: 20
 };
 
-const testUsers: Contact[] = [
-  {
-    phoneNumber: '9175730795',
-    name: 'Brendan Lim'
-  },
-  {
-    phoneNumber: '2016824408',
-    name: 'Eric Hoffman'
-  },
-  {
-    phoneNumber: '7326476905',
-    name: 'Grace Ng'
-  },
-  {
-    phoneNumber: '7187107933',
-    name: 'Chelsea Otakan'
-  }
-];
-const testNotUsers: Contact[] = [
-  {
-    phoneNumber: '7185525029',
-    name: 'Kerem Suer'
-  },
-  {
-    phoneNumber: '2038859211',
-    name: 'Raquel Parrado'
-  },
-  {
-    phoneNumber: '5513586613',
-    name: 'James Anderson'
-  }
-];
-
-const allUsers: String[] = [
-  'Brendan Lim',
-  'Eric Hoffman',
-  'Grace Ng',
-  'Chelsea Otakan',
-  'Kerem Suer',
-  'Raquel Parrado',
-  'James Anderson'
-];
-
 // interface Props
+interface ContactWithUserId extends Contact {
+  userId: string;
+}
+
+interface Props {
+  users: ContactWithUserId[];
+  notUsers: Contact[];
+  allUsers: String[];
+}
 
 class ContactsList extends React.Component<Props, {}> {
   state = {
@@ -65,27 +30,7 @@ class ContactsList extends React.Component<Props, {}> {
 
   handleRequest = (chosenRequest: string, index: number) => {
     if (chosenRequest.length > 0) {
-      let targetUser: Contact[] = [];
-      let targetNotUser: Contact[] = [];
-      let flag: number = 0;
-
-      testUsers.map((user: Contact) => {
-        if (user.name === chosenRequest) {
-          targetUser.push(user);
-          flag = 1;
-        }
-      });
-
-      if (flag !== 1) {
-        testNotUsers.map((user: Contact) => {
-          if (user.name === chosenRequest) {
-            targetNotUser.push(user);
-          }
-        });
-      }
-      this.setState({ users: targetUser, notUsers: targetNotUser });
-    } else {
-      this.setState({ users: testUsers, notUsers: testNotUsers });
+      this.setState({ filterValue: chosenRequest });
     }
     console.log(chosenRequest.length);
     return index;
@@ -93,7 +38,7 @@ class ContactsList extends React.Component<Props, {}> {
 
   handleUpdate = (searchText: string, dataSource: any[]) => {
     if (searchText.length === 0) {
-      this.setState({ users: testUsers, notUsers: testNotUsers });
+      this.setState({ filterValue: '' });
     }
     console.log(dataSource.length);
   };
@@ -111,7 +56,7 @@ class ContactsList extends React.Component<Props, {}> {
         <AutoComplete
           floatingLabelText="Search"
           filter={AutoComplete.fuzzyFilter}
-          dataSource={allUsers}
+          dataSource={this.props.allUsers}
           maxSearchResults={5}
           onNewRequest={this.handleRequest}
           onUpdateInput={this.handleUpdate}
@@ -154,6 +99,7 @@ import { StoreState } from '../types/index';
 const mapStateToProps = (state: StoreState) => {
   const users: ContactWithUserId[] = [];
   const notUsers: Contact[] = [];
+  const allUsers: String[] = [];
   const phoneNumbers = Object.keys(state.phoneNumberToContact);
   for (let phoneNumber of phoneNumbers) {
     const contact = state.phoneNumberToContact[phoneNumber];
@@ -161,15 +107,19 @@ const mapStateToProps = (state: StoreState) => {
       state.userIdsAndPhoneNumbers.phoneNumberToUserId[phoneNumber];
     if (userId) {
       users.push({ ...contact, userId: userId });
+      allUsers.push(contact.name);
     } else {
       notUsers.push(contact);
+      allUsers.push(contact.name);
     }
   }
+
   users.sort((c1, c2) => c1.name.localeCompare(c2.name));
   notUsers.sort((c1, c2) => c1.name.localeCompare(c2.name));
   return {
     users,
-    notUsers
+    notUsers,
+    allUsers
   };
 };
 // Later this will take dispatch: any as argument
