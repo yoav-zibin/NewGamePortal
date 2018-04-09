f0855a1774c64719429b326141724b2e5dd6b38a
Herbert Li <hl1785@nyu.edu>
2018-04-08 21:47:19 -0400

    Initial Commit for Resizing Board
    - Keep track of window dimension
    - add handler for resize event
    - throttle handler (to prevent lag)
    - redo x,y,width,height calcs for board and pieces
    - TODO: take into account height of app bar and video area

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -23,6 +23,8 @@ interface BoardProps {
 
 interface BoardState {
   showCardOptions: boolean;
+  innerWidth: number;
+  innerHeight: number;
 }
 
 /**
@@ -41,11 +43,15 @@ class Board extends React.Component<BoardProps, BoardState> {
   matchInfo: MatchInfo;
   gameSpec: GameSpec;
   helper: MatchStateHelper;
+  throttled: boolean;
+  delay: number;
 
   constructor(props: BoardProps) {
     super(props);
     this.state = {
-      showCardOptions: false
+      showCardOptions: false,
+      innerHeight: window.innerHeight,
+      innerWidth: window.innerWidth
     };
     this.tooltipPosition = {
       x: 0,
@@ -58,6 +64,37 @@ class Board extends React.Component<BoardProps, BoardState> {
       this.props.myUserId
     );
     this.helper = new MatchStateHelper(this.matchInfo);
+
+    // for throttling window resize event
+    this.throttled = false;
+  }
+
+  // resize the board (also for correctly displaying on mobile)
+  componentDidMount() {
+    window.addEventListener('resize', () => {
+      this.handleResize();
+    });
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', () => {
+      this.handleResize();
+    });
+  }
+
+  handleResize() {
+    if (!this.throttled) {
+      this.getDimensions();
+      this.throttled = true;
+      setTimeout(() => (this.throttled = false), 250);
+    }
+  }
+
+  getDimensions() {
+    this.setState({
+      innerWidth: window.innerWidth,
+      innerHeight: window.innerHeight
+    });
   }
 
   // check if a card belongs to a deck
@@ -101,11 +138,17 @@ class Board extends React.Component<BoardProps, BoardState> {
       'canvasImage' + index
     ] as CanvasImage).imageNode.getAbsolutePosition();
 
-    let width = this.gameSpec.board.width;
-    let height = this.gameSpec.board.height;
-    let x = position.x / width * 100;
-    let y = position.y / height * 100;
+    const width = this.gameSpec.board.width;
+    const height = this.gameSpec.board.height;
+    const ratio = Math.min(
+      this.state.innerWidth / width,
+      this.state.innerHeight / height
+    );
 
+    const x = position.x / ratio / width * 100;
+    const y = position.y / ratio / height * 100;
+
+    console.log(x, y);
     const match: MatchInfo = this.matchInfo;
     this.helper.dragTo(index, x, y);
     ourFirebase.updatePieceState(match, index);
@@ -172,12 +215,17 @@ class Board extends React.Component<BoardProps, BoardState> {
     // TODO: Complete layer for board
     let boardImage = this.gameSpec.board.downloadURL;
     // TODO: handle resizing so everything fits in the screen
-    let width = this.gameSpec.board.width;
-    let height = this.gameSpec.board.height;
+    const width = this.gameSpec.board.width;
+    const height = this.gameSpec.board.height;
+    const ratio = Math.min(
+      this.state.innerWidth / width,
+      this.state.innerHeight / height
+    );
+
     let boardLayer = (
       <CanvasImage
-        height={height}
-        width={width}
+        height={height * ratio}
+        width={width * ratio}
         src={boardImage}
         onClick={() => this.hideCardOptions()}
       />
@@ -220,12 +268,10 @@ class Board extends React.Component<BoardProps, BoardState> {
               this.toggleCardOptions('canvasImage' + index, index);
             }
           }}
-          height={
-            pieceSpec.element.height * height / this.gameSpec.board.height
-          }
-          width={pieceSpec.element.width * width / this.gameSpec.board.width}
-          x={piece.x * width / 100}
-          y={piece.y * height / 100}
+          height={pieceSpec.element.height * ratio}
+          width={pieceSpec.element.width * ratio}
+          x={piece.x * width / 100 * ratio}
+          y={piece.y * height / 100 * ratio}
           src={imageSrc}
           onDragStart={() => {
             this.hideCardOptions();
@@ -302,7 +348,7 @@ class Board extends React.Component<BoardProps, BoardState> {
     return (
       <div style={{ position: 'relative' }}>
         {toolTipLayer}
-        <Stage width={width} height={height}>
+        <Stage width={width * ratio} height={height * ratio}>
           <Layer ref={() => 'boardLayer'}>{boardLayer}</Layer>
           <Layer ref={() => 'piecesLayer'}>{piecesLayer}</Layer>
         </Stage>
