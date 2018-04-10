215fdafc26e5bbb16f0813aaf0f27cdd2cab4a88
Radhika Mattoo <rm3485@nyu.edu>
2018-04-09 20:09:15 -0400

    have correct name mapping for appheader match

--- src/AppHeader.tsx
+++ src/AppHeader.tsx
@@ -3,12 +3,22 @@ import AppBar from 'material-ui/AppBar';
 import FloatingActionButton from 'material-ui/FloatingActionButton';
 import NavigationArrowBack from 'material-ui/svg-icons/navigation/arrow-back';
 import { StringIndexer } from './types';
-import { MatchInfo, StoreState, GameInfo } from './types';
+import {
+  MatchInfo,
+  StoreState,
+  GameInfo,
+  UserIdsAndPhoneNumbers,
+  PhoneNumberToContact,
+  MyUser
+} from './types';
 import { connect } from 'react-redux';
 
 interface Props {
   matchesList: MatchInfo[];
   gamesList: GameInfo[];
+  userIdsAndPhoneNumbers: UserIdsAndPhoneNumbers;
+  phoneNumberToContact: PhoneNumberToContact;
+  myUser: MyUser;
 }
 
 class AppHeader extends React.Component<Props, {}> {
@@ -27,19 +37,35 @@ class AppHeader extends React.Component<Props, {}> {
       return result;
     } else if (pathname.startsWith('/matches/')) {
       let matchId = pathname.split('/')[2];
-      let title = '';
+      let title = ''; // String to build
+
+      // Get corresponding info for selected match
       this.props.matchesList.forEach((match: MatchInfo) => {
         if (match.matchId === matchId) {
-          console.log('Found matchId');
           const game: GameInfo = match.game;
           title += game.gameName;
+          console.log(
+            'PARTICIPANTUDERIDS LENGTH: ',
+            match.participantsUserIds.length
+          );
+          console.log('MY USER ID: ', this.props.myUser.myUserId);
+          // Is the game multiplayer? If so convert ID --> Phone --> Contact
           if (match.participantsUserIds.length > 1) {
             title += ' with ';
-            match.participantsUserIds.forEach((participantId: String) => {
-              title += participantId;
+            match.participantsUserIds.forEach((participantId: string) => {
+              // Exclude myself
+              if (participantId !== this.props.myUser.myUserId) {
+                console.log('PARTICIPANTID', participantId);
+                const phoneNumber = this.props.userIdsAndPhoneNumbers
+                  .userIdToPhoneNumber[participantId];
+                console.log('PHONE NUMBER: ', phoneNumber);
+                const contact = this.props.phoneNumberToContact[phoneNumber];
+                console.log('CONTACT NAME: ', contact);
+                title += contact.name;
+              }
             });
-          }
-        }
+          } // End length if
+        } // End matchId if
       });
       return title;
     } else {
@@ -69,7 +95,10 @@ class AppHeader extends React.Component<Props, {}> {
 
 const mapStateToProps = (state: StoreState) => ({
   matchesList: state.matchesList,
-  gamesList: state.gamesList
+  gamesList: state.gamesList,
+  userIdsAndPhoneNumbers: state.userIdsAndPhoneNumbers,
+  phoneNumberToContact: state.phoneNumberToContact,
+  myUser: state.myUser
 });
 
 export default connect(mapStateToProps)(AppHeader);
