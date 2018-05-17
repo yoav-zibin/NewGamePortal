e7c9ec373d28738c3fa29d11e0304259e400f787
vivianros <32103410+vivianros@users.noreply.github.com>
2018-05-17 01:48:53 -0400

    add cards need to be hidden

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -335,7 +335,13 @@ export namespace ourFirebase {
       '-L01xDqdP8hN1PCJuwI8', // Texas Hold 'em
       '-L01xupWIg2jOCp9Sh_K', // Crazy Eights
       '-L0DEfpWO-G2hX3Wcp8-', // Spades
-      '-L0r6qHkR2fkeCk6B7zB' // Bananagrams
+      '-L0r6qHkR2fkeCk6B7zB', // Bananagrams
+      '-L-7byJiaazYQ5V5i2hW', // Sevens
+      '-L-8rUonEDrh-IkJk9nk', // Dou dizhu
+      '-L-9-GuzOZJ6sRAVCh6b', // Five Card Stud
+      '-L-9qTVLsumaP9TBL9_O', // diaoyu (changed name: Go Fish)
+      '-L-lw5cA3nHJlK8Lc5V9', // Dueling Nobles
+      '-L-mhJby9spVzuJTrwti', // Dominion
     ];
     for (let gameSpecId of switchCardImages) {
       const spec = gameSpecs.gameSpecIdToGameSpec[gameSpecId];
